296fbd893317ced2e1e6083fdcb2156a14751930
Adam Payne <ajpayne238@gmail.com>
2019-12-10 13:28:25 -0500

    added routing and wrapped app in browserrouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 
 import React from 'react'
-// import LinkList from './LinkList'
+import LinkList from './LinkList'
 import CreateLink from './CreateLink'
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
 import '../styles/App.css'
 
 function App() {
   return (
-    <div className="App">
-      {/* <LinkList /> */}
-      <CreateLink />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
 
 //1
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
+
 
 //2 
 const httpLink = createHttpLink({
@@ -23,9 +25,11 @@ const client = new ApolloClient({
 
 //4
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById('root')
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>,
+document.getElementById('root')
 )
-serviceWorker.unregister();
+serviceWorker.unregister()
