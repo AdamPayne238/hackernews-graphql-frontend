a2c52a4f4077f6e33801212397d42b79770a2905
Adam Payne <ajpayne238@gmail.com>
2019-12-11 15:46:18 -0500

    search bar fully functional

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,13 +4,41 @@ import { withApollo } from 'react-apollo'
 import gql from 'graphql-tag'
 import Link from './Link'
 
-const Search = () => {
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+const Search = (props) => {
 
     const [ links, setLinks ] = useState([])
     const [ filter, setFilter ] = useState('')
 
     const _executeSearch = async () => {
-        // .. youll complete this later
+        const result = await props.client.query({
+            query: FEED_SEARCH_QUERY,
+            variables: { filter },
+        })
+        const links = result.data.feed.links
+        setLinks( links )
     }
 
     return (
@@ -33,5 +61,7 @@ const Search = () => {
         </div>
     )
 }
+
+
 
 export default withApollo(Search)
